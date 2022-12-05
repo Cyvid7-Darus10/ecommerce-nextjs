ae5d09848053e2813654e09b867a0b948fd183b8
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-12-05 09:14:20 +0800

    added the data.js

---
+++ utils/data.js
@@ -0,0 +1,190 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "CCTV",
+      slug: "cctv",
+      description: "CCTV",
+      price: 100,
+      image: "/images/cctv.jpg",
+      category: "cctv",
+      countInStock: 10,
+      brand: "Cabsfour",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 2,
+      name: "CCTV 2",
+      slug: "cctv-2",
+      description: "CCTV 2",
+      price: 100,
+      image: "/images/cctv.jpg",
+      category: "cctv",
+      countInStock: 10,
+      brand: "Cabsfour",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 1,
+      name: "CCTV",
+      slug: "cctv",
+      description: "CCTV",
+      price: 100,
+      image: "/images/cctv.jpg",
+      category: "cctv",
+      countInStock: 10,
+      brand: "Cabsfour",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 2,
+      name: "CCTV 2",
+      slug: "cctv-2",
+      description: "CCTV 2",
+      price: 100,
+      image: "/images/cctv.jpg",
+      category: "cctv",
+      countInStock: 10,
+      brand: "Cabsfour",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 1,
+      name: "CCTV",
+      slug: "cctv",
+      description: "CCTV",
+      price: 100,
+      image: "/images/cctv.jpg",
+      category: "cctv",
+      countInStock: 10,
+      brand: "Cabsfour",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 2,
+      name: "CCTV 2",
+      slug: "cctv-2",
+      description: "CCTV 2",
+      price: 100,
+      image: "/images/cctv.jpg",
+      category: "cctv",
+      countInStock: 10,
+      brand: "Cabsfour",
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ],
+
+  services: [
+    {
+      id: 1,
+      name: "CCTV",
+      alt: "CCTV",
+      information:
+        "With the increasing demand on CCTV surveillance system, Safetyzone offers wide range of surveillance cameras and recorders that will fit your security requirements and budget.",
+      image: "/images/service/services1.png",
+    },
+    {
+      id: 2,
+      name: "Fire Alarm System",
+      alt: "Fire Alarm System",
+      information:
+        "Fire alarm is designed to detect the unwanted presence of fire by monitoring environmental changes associated It can be automatically actuated, manually actuated or both automatic.",
+      image: "/images/service/services2.png",
+    },
+    {
+      id: 3,
+      name: "Point of Sale System",
+      alt: "Point of Sale System",
+      information:
+        "point-of-sale that uses biometric authentication based on physical characteristics to identify the user and authorize the deduction of funds from a bank account.",
+      image: "/images/service/services3.png",
+    },
+    {
+      id: 4,
+      name: "Network Cabling",
+      alt: "Network Cabling",
+      information:
+        " Structured cabling is an infrastructure that’s made up of smaller, standardized elements. You can install structured cabling systems in a building or across a campus.",
+      image: "/images/service/services4.png",
+    },
+    {
+      id: 5,
+      name: "Biometrics",
+      alt: "Biometrics",
+      information:
+        "A measurable physical characteristic or personal behavioral trait used to recognize the identity, or verify the claimed identity, of an applicant. Facial images, fingerprints, and iris scan.",
+      image: "/images/service/services5.png",
+    },
+    {
+      id: 6,
+      name: "Computer Repair",
+      alt: "Computer Repair",
+      information:
+        "Computer repair is a broad field encompassing many tools, techniques and procedures used to repair computer hardware, software or network/Internet problems.",
+      image: "/images/service/services6.png",
+    },
+    {
+      id: 7,
+      name: "Access Control",
+      alt: "Access Control",
+      information:
+        "Its purpose is to limit access to people who are authorized to enter an organization. It uses door readers that detect the unique identification numbers provided by credentials.",
+      image: "/images/service/services7.png",
+    },
+    {
+      id: 8,
+      name: "Public Address System",
+      alt: "Public Address System",
+      information:
+        "made up of an input source like a microphone, amplifiers, speakers and a controller.It allows to communicate from central panel to multiple or single areas.",
+      image: "/images/service/services8.png",
+    },
+  ],
+
+  clients: [
+    {
+      id: 1,
+      name: "Althea",
+      image: "/images/logo/Bakeshop _ Goodies-logo.png",
+    },
+    {
+      id: 2,
+      name: "Circle Plus Pharmacy",
+      image: "/images/logo/Circle_Plus_Pharmacy-logo.png",
+    },
+    {
+      id: 3,
+      name: "LTO",
+      image: "/images/logo/lto-logo.png",
+    },
+    {
+      id: 4,
+      name: "Triwave Corporation",
+      image: "/images/logo/triwavecorp-logo.png",
+    },
+    {
+      id: 5,
+      name: "Kimchi Minimart",
+      image: "/images/logo/kimchi_minimart-logo.png",
+    },
+    {
+      id: 6,
+      name: "Peak Sun Trucking",
+      image: "/images/logo/Peaksun Trucking-logo.png",
+    },
+    {
+      id: 7,
+      name: "Ecosystem Technologies International Warehouse",
+      image:
+        "/images/logo/Ecosystem Technologies International Warehouse-logo.png",
+    },
+  ],
+};
+
+export default data;
