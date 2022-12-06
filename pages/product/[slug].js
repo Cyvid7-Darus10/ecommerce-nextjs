9bcd37e4715a355545cef6e25f356470da09bcaa
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-12-07 05:35:47 +0800

    created page for specific item

---
+++ pages/product/[slug].js
@@ -0,0 +1,99 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+import data from "../../utils/data";
+
+import Image from "next/image";
+
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+import Link from "next/link";
+
+export default function ProductScreen() {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug);
+  if (!product) {
+    return <div>Product Not Found</div>;
+  }
+
+  const randomProducts = data.products.sort(() => Math.random() - 0.5);
+  const randomFourProducts = randomProducts.slice(0, 4);
+
+  return (
+    <>
+      <Layout title='Product'>
+        <div className='container px-0 mx-0 '>
+          <div className='flex items-center py-2 px-5'>
+            <Link href='/products'>
+              <div className='flex items-center'>
+                <ArrowBackIcon className='text-[#f44336] text-2xl mr-2' />
+                <p className='text-[#f44336] text-xl'>Back to Products</p>
+              </div>
+            </Link>
+          </div>
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 py-12 px-5'>
+            <div className='flex flex-col items-center'>
+              <div className='w-100 h-100'>
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  width={500}
+                  height={500}
+                  className='w-100 h-100'
+                />
+              </div>
+            </div>
+            <div className='flex flex-col items-center'>
+              <div className='w-100 h-100'>
+                <h1 className='text-3xl font-semibold'>{product.name}</h1>
+                <p className='text-2xl font-semibold py-2'>
+                  Price: ₱{product.price}
+                </p>
+                <p className='text-xl font-semibold py-2'>Description:</p>
+                <p className='text-lg py-2'>{product.description}</p>
+              </div>
+
+              <button className='bg-[#f44336] text-white px-5 py-2 rounded-md mt-auto'>
+                <AddShoppingCartIcon className='text-white text-2xl' />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+
+          <p className='text-xl font-semibold px-5 py-5'>Similar Items</p>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pb-12 px-5'>
+            {randomFourProducts.map((product) => (
+              <div
+                className='flex flex-col items-center py-10 px-5 border border-gray-200 rounded-md shadow-md'
+                key={product.id}
+              >
+                <Link href={`/product/${product.slug}`}>
+                  <div className='w-100 h-100 hover:transform hover:scale-110 transition duration-500 ease-in-out'>
+                    <Image
+                      src={product.image}
+                      alt='CCTV'
+                      className=''
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                </Link>
+
+                <p className='text-xl font-bold text-left mt-auto'>
+                  {product.name}
+                </p>
+                <p className='py-5 text-[#f44336] mt-auto'>₱ {product.price}</p>
+                <button className='bg-[#f44336] text-white px-5 py-2 rounded-md mt-auto'>
+                  <AddShoppingCartIcon className='text-white text-2xl' />
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
