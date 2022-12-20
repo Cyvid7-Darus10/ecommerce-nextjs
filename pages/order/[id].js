6d7507d8d95e1655adede0d73b851e0cfcd18aae
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-12-21 02:31:17 +0800

    created order screen

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -2,79 +2,74 @@ import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Cookies from "js-cookie";
-import React, { useContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import CheckoutWizard from "../components/CheckoutWizard";
-import Layout from "../components/Layout";
-import { getError } from "../utils/error";
-import { Store } from "../utils/Store";
-import { Card, CardHeader, CardBody, Button } from "@material-tailwind/react";
+import React, { useEffect, useReducer } from "react";
+import Layout from "../../components/Layout";
+import { getError } from "../../utils/error";
+import { Card, CardHeader, CardBody } from "@material-tailwind/react";
 
-export default function PlaceOrderScreen() {
-  const { state, dispatch } = useContext(Store);
-  const { cart } = state;
-  const { cartItems, shippingAddress, paymentMethod } = cart;
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, order: action.payload, error: "" };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
 
-  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+export default function OrderScreen() {
+  const { query } = useRouter();
+  const orderId = query.id;
 
-  const itemsPrice = round2(
-    cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
-  ); // 123.4567 => 123.46
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: "",
+  });
 
-  const shippingPrice = itemsPrice > 200 ? 0 : 15;
-  const taxPrice = round2(itemsPrice * 0.15);
-  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
-
-  const router = useRouter();
   useEffect(() => {
-    if (!paymentMethod) {
-      router.push("/payment");
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`/api/orders/${orderId}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
     }
+  }, [order, orderId]);
 
-    return () => {};
-  }, [paymentMethod, router]);
-
-  const [loading, setLoading] = useState(false);
-
-  const placeOrderHandler = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.post("/api/orders", {
-        orderItems: cartItems,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        shippingPrice,
-        taxPrice,
-        totalPrice,
-      });
-      setLoading(false);
-      dispatch({ type: "CART_CLEAR_ITEMS" });
-      Cookies.set(
-        "cart",
-        JSON.stringify({
-          ...cart,
-          cartItems: [],
-        })
-      );
-      router.push(`/order/${data._id}`);
-    } catch (err) {
-      setLoading(false);
-      toast.error(getError(err));
-    }
-  };
+  const {
+    shippingAddress,
+    paymentMethod,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    isPaid,
+    paidAt,
+    isDelivered,
+    deliveredAt,
+  } = order;
 
   return (
-    <Layout title='Place Order' smallHeader={true}>
+    <Layout title='Order' smallHeader={true}>
       <div className='p-4 m-5 w-full'>
-        <CheckoutWizard activeStep={3} />
-        {cartItems.length === 0 ? (
+        <p className='text-xl text-center'>Order ID: {orderId}</p>
+        {loading ? (
           <div className='text-center'>
-            <span>Cart is empty. </span>
-            <Link href='/products' className='text-blue-500'>
-              Go shopping
-            </Link>
+            <span>Loading...</span>
+          </div>
+        ) : error ? (
+          <div className='text-center'>
+            <span className='text-red-500'>{error}</span>
           </div>
         ) : (
           <div className='grid md:grid-cols-4 md:gap-5'>
@@ -89,11 +84,13 @@ export default function PlaceOrderScreen() {
                     {shippingAddress.city}, {shippingAddress.postalCode},{" "}
                     {shippingAddress.country}
                   </div>
-                  <div>
-                    <Link href='/shipping' className='text-blue-500'>
-                      Edit
-                    </Link>
-                  </div>
+                  {isDelivered ? (
+                    <div className='alert-success'>
+                      Delivered at {deliveredAt}
+                    </div>
+                  ) : (
+                    <div className='text-red-500'>Not delivered</div>
+                  )}
                 </CardBody>
               </Card>
               <Card className='shadow-none'>
@@ -102,11 +99,11 @@ export default function PlaceOrderScreen() {
                 </CardHeader>
                 <CardBody>
                   <div>{paymentMethod}</div>
-                  <div>
-                    <Link href='/payment' className='text-blue-500'>
-                      Edit
-                    </Link>
-                  </div>
+                  {isPaid ? (
+                    <div className='alert-success'>Paid at {paidAt}</div>
+                  ) : (
+                    <div className='text-red-500'>Not paid</div>
+                  )}
                 </CardBody>
               </Card>
               <Card className='shadow-none'>
@@ -124,11 +121,11 @@ export default function PlaceOrderScreen() {
                       </tr>
                     </thead>
                     <tbody>
-                      {cartItems.map((item) => (
+                      {orderItems.map((item) => (
                         <tr key={item._id} className='border-b'>
                           <td>
                             <Link
-                              href={`/product/₱{item.slug}`}
+                              href={`/product/${item.slug}`}
                               className='flex items-center'
                             >
                               <Image
@@ -142,19 +139,14 @@ export default function PlaceOrderScreen() {
                             </Link>
                           </td>
                           <td className=' p-5 text-right'>{item.quantity}</td>
-                          <td className='p-5 text-right'>₱{item.price}</td>
+                          <td className='p-5 text-right'>${item.price}</td>
                           <td className='p-5 text-right'>
-                            ₱{item.quantity * item.price}
+                            ${item.quantity * item.price}
                           </td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
-                  <div>
-                    <Link href='/cart' className='text-blue-500'>
-                      Edit
-                    </Link>
-                  </div>
                 </CardBody>
               </Card>
             </div>
@@ -168,36 +160,26 @@ export default function PlaceOrderScreen() {
                     <li>
                       <div className='mb-2 flex justify-between'>
                         <div>Items</div>
-                        <div>₱{itemsPrice}</div>
+                        <div>${itemsPrice}</div>
                       </div>
                     </li>
                     <li>
                       <div className='mb-2 flex justify-between'>
                         <div>Tax</div>
-                        <div>₱{taxPrice}</div>
+                        <div>${taxPrice}</div>
                       </div>
                     </li>
                     <li>
                       <div className='mb-2 flex justify-between'>
                         <div>Shipping</div>
-                        <div>₱{shippingPrice}</div>
+                        <div>${shippingPrice}</div>
                       </div>
                     </li>
                     <li>
                       <div className='mb-2 flex justify-between'>
                         <div>Total</div>
-                        <div>₱{totalPrice}</div>
+                        <div>${totalPrice}</div>
                       </div>
-                    </li>
-                    <li>
-                      <Button
-                        color='red'
-                        disabled={loading}
-                        onClick={placeOrderHandler}
-                        className='primary-button w-full'
-                      >
-                        {loading ? "Loading..." : "Place Order"}
-                      </Button>
                     </li>
                   </ul>
                 </CardBody>
@@ -210,4 +192,4 @@ export default function PlaceOrderScreen() {
   );
 }
 
-PlaceOrderScreen.auth = true;
+OrderScreen.auth = true;
