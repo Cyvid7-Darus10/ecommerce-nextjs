1425aa62533dfd63b87c2d44bdb98b7ae88a48fc
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-03-04 16:01:56 +0800

    feat: Added the fixed receipt

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -184,106 +184,275 @@ export default function OrderScreen() {
     document.body.innerHTML = originalContents;
   };
 
-  function FormattedDate(props) {
-    const date = new Date(props.date);
-    const formattedDate = `${date.getDate()}/${
-      date.getMonth() + 1
-    }/${date.getFullYear()}`;
-    // Format the date as dd/mm/yyyy
-
-    return <span>{formattedDate}</span>;
-  }
-
   return (
-    <div id='receipt'>
-      <div className='flex flex-col items-center justify-center w-full h-full'>
-        <div className='flex flex-col w-full h-full items-center'>
-          <h1 className='text-2xl'>
-            <strong>CABSFOR SECURITY SYSTEMS SERVICES</strong>
-          </h1>
-          <p>A Ricardo Bagac, Bataan</p>
-          <p>
-            <strong>LEMUILLE T. CABANTAV -</strong> Prop.
-          </p>
-          <p>
-            NON VAT Reg. TIN: <strong>481-999-558-000</strong>
-          </p>
-
-          <hr className='w-full border-2 border-black my-2 border-dashed' />
-
-          <div className='italic text-center'> {paidAt} </div>
-          <div className='italic text-center'> Sale ID: {order._id} </div>
-        </div>
+    <Layout title='Order' smallHeader={true}>
+      <div className='p-4 m-5 w-full'>
+        <p className='text-xl text-center'>Order ID: {orderId}</p>
+        {loading ? (
+          <div className='text-center'>
+            <span>Loading...</span>
+          </div>
+        ) : error ? (
+          <div className='text-center'>
+            <span className='text-red-500'>{error}</span>
+          </div>
+        ) : (
+          <div className='grid md:grid-cols-4 md:gap-5'>
+            <div className='overflow-x-auto md:col-span-3'>
+              <Card className='shadow-none'>
+                <CardHeader floated={false} className='text-lg rounded-sm p-3'>
+                  Shipping Address
+                </CardHeader>
+                <CardBody>
+                  <div>
+                    {shippingAddress.fullName}, {shippingAddress.address},{" "}
+                    {shippingAddress.city}, {shippingAddress.postalCode},{" "}
+                    {shippingAddress.country}
+                  </div>
+                  {isDelivered ? (
+                    <div className='text-green-500'>
+                      Delivered at {deliveredAt}
+                    </div>
+                  ) : (
+                    <div className='text-red-500'>Not delivered</div>
+                  )}
+                </CardBody>
+              </Card>
+              <Card className='shadow-none'>
+                <CardHeader floated={false} className='text-lg rounded-sm p-3'>
+                  Payment Method
+                </CardHeader>
+                <CardBody>
+                  <div>{paymentMethod}</div>
+                  {isPaid ? (
+                    <div className='text-green-500'>Paid at {paidAt}</div>
+                  ) : (
+                    <div className='text-red-500'>Not paid</div>
+                  )}
+                </CardBody>
+              </Card>
+              <Card className='shadow-none'>
+                <CardHeader floated={false} className='text-lg rounded-sm p-3'>
+                  Order Items
+                </CardHeader>
+                <CardBody>
+                  <table className='min-w-full mb-1'>
+                    <thead className='border-b'>
+                      <tr>
+                        <th className='px-5 text-left'>Item</th>
+                        <th className='p-5 text-right'>Quantity</th>
+                        <th className='p-5 text-right'>Price</th>
+                        <th className='p-5 text-right'>Subtotal</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orderItems?.map((item) => (
+                        <tr key={item._id} className='border-b'>
+                          <td>
+                            <Link
+                              href={`/product/${item.slug}`}
+                              className='flex items-center'
+                            >
+                              <Image
+                                src={item.image}
+                                alt={item.name}
+                                width={50}
+                                height={50}
+                              ></Image>
+                              &nbsp;
+                              {item.name}
+                            </Link>
+                          </td>
+                          <td className=' p-5 text-right'>{item.quantity}</td>
+                          <td className='p-5 text-right'>
+                            ₱{formatNumber(item.price)}
+                          </td>
+                          <td className='p-5 text-right'>
+                            ₱{formatNumber(item.quantity * item.price)}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </CardBody>
+              </Card>
+            </div>
+            <div>
+              <Card className='shadow-none'>
+                <CardHeader floated={false} className='text-lg rounded-sm p-3'>
+                  Order Summary
+                </CardHeader>
+                <CardBody>
+                  <ul>
+                    <li>
+                      <div className='mb-2 flex justify-between'>
+                        <div>Items</div>
+                        <div>₱{formatNumber(itemsPrice)}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className='mb-2 flex justify-between'>
+                        <div>Tax</div>
+                        <div>₱{formatNumber(taxPrice)}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className='mb-2 flex justify-between'>
+                        <div>Shipping</div>
+                        <div>₱{formatNumber(shippingPrice)}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className='mb-2 flex justify-between'>
+                        <div>Total</div>
+                        <div>₱{formatNumber(totalPrice)}</div>
+                      </div>
+                    </li>
+                    {!isPaid && (
+                      <li>
+                        {isPending ? (
+                          <div>Loading...</div>
+                        ) : (
+                          <div className='w-full'>
+                            <PayPalButtons
+                              createOrder={createOrder}
+                              onApprove={onApprove}
+                              onError={onError}
+                            ></PayPalButtons>
+                          </div>
+                        )}
+                        {loadingPay && <div>Loading...</div>}
+                      </li>
+                    )}
+                    {!isPaid && (
+                      <li>
+                        <Button onClick={nextPay} className='w-full'>
+                          Next Pay (Gcash, Paymaya, etc.)
+                        </Button>
+                      </li>
+                    )}
+                    {session.user.isAdmin &&
+                      order.isPaid &&
+                      !order.isDelivered && (
+                        <li>
+                          {loadingDeliver && <div>Loading...</div>}
+                          <Button
+                            className='primary-button w-full'
+                            onClick={deliverOrderHandler}
+                          >
+                            Deliver Order
+                          </Button>
+                        </li>
+                      )}
+                    {order.isPaid && order.isDelivered && (
+                      <li>
+                        {loadingDeliver && <div>Loading...</div>}
+                        <Button
+                          className='primary-button w-full'
+                          onClick={printReceipt}
+                        >
+                          Print Receipt
+                        </Button>
+                      </li>
+                    )}
+                  </ul>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        )}
       </div>
-      <hr className='w-full border-2 border-black my-2 border-dashed' />
+      <div id='receipt' className='hidden'>
+        <div className='flex flex-col items-center justify-center w-full h-full'>
+          <div className='flex flex-col w-full h-full items-center'>
+            <h1 className='text-2xl'>
+              <strong>CABSFOR SECURITY SYSTEMS SERVICES</strong>
+            </h1>
+            <p>A Ricardo Bagac, Bataan</p>
+            <p>
+              <strong>LEMUILLE T. CABANTAV -</strong> Prop.
+            </p>
+            <p>
+              NON VAT Reg. TIN: <strong>481-999-558-000</strong>
+            </p>
 
-      <div className='flex flex-col justify-start items-start'>
-        <div className='text-left w-1/2'>
-          Invoice To: {shippingAddress?.fullName}
+            <hr className='w-full border-2 border-black my-2 border-dashed' />
+
+            <div className='italic text-center'> {paidAt} </div>
+            <div className='italic text-center'> Sale ID: {order._id} </div>
+          </div>
         </div>
-        <div className='flex items-start'>
-          {shippingAddress?.address}, {shippingAddress?.city},{" "}
-          {shippingAddress?.postalCode}, {shippingAddress?.country}{" "}
+        <hr className='w-full border-2 border-black my-2 border-dashed' />
+
+        <div className='flex flex-col justify-start items-start'>
+          <div className='text-left w-1/2'>
+            Invoice To: {shippingAddress?.fullName}
+          </div>
+          <div className='flex items-start'>
+            {shippingAddress?.address}, {shippingAddress?.city},{" "}
+            {shippingAddress?.postalCode}, {shippingAddress?.country}{" "}
+          </div>
+          <div className='flex items-start'>
+            {shippingAddress?.contactNumber}
+          </div>
         </div>
-        <div className='flex items-start'>{shippingAddress?.contactNumber}</div>
-      </div>
 
-      <hr className='w-full border-2 border-black my-2 border-dashed' />
+        <hr className='w-full border-2 border-black my-2 border-dashed' />
 
-      <div className='flex justify-center items-center w-screen'>
-        <table className='table w-full'>
-          <tbody>
-            <tr className='border-b-2'>
-              <td className='text-center'>Item Name</td>
-              <td className='text-center'>Price</td>
-              <td className='text-center'>Qty.</td>
-              <td className='text-center'>Total</td>
-            </tr>
-            {orderItems?.map((item, key) => (
-              <tr key={key} className='border-b-2 border-t-2'>
-                <td className='w-1/2'>{item.name}</td>
-                <td className='w-1/4 text-center'>
-                  ₱{formatNumber(item.quantity * item.price)}
-                </td>
-                <td className='w-1/4 text-center'>{item.quantity}</td>
-                <td>₱{formatNumber(item.quantity * item.price)}</td>
+        <div className='flex justify-center items-center w-screen'>
+          <table className='table w-full'>
+            <tbody>
+              <tr className='border-b-2'>
+                <td className='text-center'>Item Name</td>
+                <td className='text-center'>Price</td>
+                <td className='text-center'>Qty.</td>
+                <td className='text-center'>Total</td>
               </tr>
-            ))}
-            <tr>
-              <td></td>
-              <td></td>
-              <td className='w-3/4 text-center'>Subtotal</td>
-              <td className='w-1/4'>₱{formatNumber(itemsPrice)}</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td className='w-3/4  text-center'>Shipping</td>
-              <td className='w-1/4  text-center'>
-                ₱{formatNumber(shippingPrice)}
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td className='w-3/4  text-center'>Tax</td>
-              <td className='w-1/4'>₱{formatNumber(taxPrice)}</td>
-            </tr>
-            <tr>
-              <td>Payment Type</td>
-              <td className='text-center'> {order.paymentMethod} </td>
-              <td className='w-3/4  text-center'>Total</td>
-              <td className='w-1/4'>₱{formatNumber(totalPrice)}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className='flex flex-col justify-center items-center text-xs mt-20'>
-        <div className='w-1/2 text-center'>
-          <p>WARRANTY AGREEMENT</p>
-          <br />
-          <p>
-            {`
+              {orderItems?.map((item, key) => (
+                <tr key={key} className='border-b-2 border-t-2'>
+                  <td className='w-1/2'>{item.name}</td>
+                  <td className='w-1/4 text-center'>
+                    ₱{formatNumber(item.quantity * item.price)}
+                  </td>
+                  <td className='w-1/4 text-center'>{item.quantity}</td>
+                  <td>₱{formatNumber(item.quantity * item.price)}</td>
+                </tr>
+              ))}
+              <tr>
+                <td></td>
+                <td></td>
+                <td className='w-3/4 text-center'>Subtotal</td>
+                <td className='w-1/4'>₱{formatNumber(itemsPrice)}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td className='w-3/4  text-center'>Shipping</td>
+                <td className='w-1/4  text-center'>
+                  ₱{formatNumber(shippingPrice)}
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td className='w-3/4  text-center'>Tax</td>
+                <td className='w-1/4'>₱{formatNumber(taxPrice)}</td>
+              </tr>
+              <tr>
+                <td>Payment Type</td>
+                <td className='text-center'> {order.paymentMethod} </td>
+                <td className='w-3/4  text-center'>Total</td>
+                <td className='w-1/4'>₱{formatNumber(totalPrice)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className='flex flex-col justify-center items-center text-xs mt-20'>
+          <div className='w-1/2 text-center'>
+            <p>WARRANTY AGREEMENT</p>
+            <br />
+            <p>
+              {`
         Cabsfour warranty it's products contained in their
         original packaging against material and workmanship defects when the
         products are used normally for their intended purposes for a period of
@@ -294,10 +463,10 @@ export default function OrderScreen() {
         To return or exchange items, you must bring the items in its original
         packaging (No packaging No return), proof of purchase Sales Receipt)
         exclusions and limitations apply*`}
-          </p>
-          <br />
-          <p>
-            {`B. Standard warranty-after seven days,
+            </p>
+            <br />
+            <p>
+              {`B. Standard warranty-after seven days,
         merchandise is no longer eligible for outright Replacement, but can
         still be sent in for warranty claims so long as within the standard
         warranty period. For warranty claim,bring the following Cabsfour Store:
@@ -310,10 +479,11 @@ export default function OrderScreen() {
         customers may opt to add cash to this store credit to purchase items of
         higher value. Cabsfour reserves the right to decline any return or
         exchange when deemed necessary.`}
-          </p>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
